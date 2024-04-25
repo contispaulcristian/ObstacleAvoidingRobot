0f5d1ccfdedd568843e9a83e9d833ac831df7b8d
Paul Contis <contispaulcristian@gmail.com>
2024-04-25 13:09:32 +0300

    GPIO Peripheral driver

---
+++ Debug/Drivers/Src/stm32f446xx_gpio_driver.d
@@ -0,0 +1,8 @@
+Drivers/Src/stm32f446xx_gpio_driver.o: \
+ ../Drivers/Src/stm32f446xx_gpio_driver.c \
+ D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx_gpio_driver.h \
+ D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx.h \
+ D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx_gpio_driver.h
+D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx_gpio_driver.h:
+D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx.h:
+D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc/stm32f446xx_gpio_driver.h:
--- Debug/Drivers/Src/subdir.mk
+++ Debug/Drivers/Src/subdir.mk
@@ -5,29 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../Drivers/Src/stm32f446xx_gpio_driver.c 
 
 OBJS += \
-./Src/main.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./Drivers/Src/stm32f446xx_gpio_driver.o 
 
 C_DEPS += \
-./Src/main.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./Drivers/Src/stm32f446xx_gpio_driver.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+Drivers/Src/%.o Drivers/Src/%.su Drivers/Src/%.cyclo: ../Drivers/Src/%.c Drivers/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -c -I../Inc -I"D:/STM32_Workspace/ObstacleAvoidingRobot/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
-clean: clean-Src
+clean: clean-Drivers-2f-Src
 
-clean-Src:
-	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+clean-Drivers-2f-Src:
+	-$(RM) ./Drivers/Src/stm32f446xx_gpio_driver.cyclo ./Drivers/Src/stm32f446xx_gpio_driver.d ./Drivers/Src/stm32f446xx_gpio_driver.o ./Drivers/Src/stm32f446xx_gpio_driver.su
 
-.PHONY: clean-Src
+.PHONY: clean-Drivers-2f-Src
 
